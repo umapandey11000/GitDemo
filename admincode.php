8e585d763f2ff0331c39d17e2b9484b7d5e3654c
umapandey11000 <umapandey11000@gmail.com>
2025-06-30 20:56:40 +0530

    second commit

--- admincode.php
+++ admincode.php
@@ -3,6 +3,7 @@ include("connection.php");
 session_start();
 $name=$_POST['name'];
 echo $name;
+echo $password;
 $password=$_POST['password'];
 echo $password;
 $query="insert into tbl_admin(user_name,password)values('$name','$password')";
